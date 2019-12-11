9330d84faaef5aaf38674f86802587fd25859876
Will Jones <willjones2010@gmail.com>
2019-12-11 18:30:26 -0500

    updated winner announcement and reset
    deleted setScreenAttribute function as well

--- pong.py
+++ pong.py
@@ -68,73 +68,6 @@ winner.showturtle()
 winner.goto(0, 200)
 
 
-def setScreenAttributes():
-    wn.title("Will's Pong Game")
-    wn.bgcolor("black")
-    wn.setup(width=800, height=600)
-    wn.tracer(0)
-    # Score
-    score_a = 0
-    score_b = 0
-
-    # Paddle A
-    paddle_a = turtle.Turtle()
-    paddle_a.speed(0)
-    paddle_a.shape("square")
-    paddle_a.color("white")
-    paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-    paddle_a.penup()
-    paddle_a.goto(-350, 0)
-
-    # Paddle B
-    paddle_b = turtle.Turtle()
-    paddle_b.speed(0)
-    paddle_b.shape("square")
-    paddle_b.color("white")
-    paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-    paddle_b.penup()
-    paddle_b.goto(350, 0)
-
-    # Ball
-    ball = turtle.Turtle()
-    ball.speed(0)
-    ball.shape("square")
-    ball.color("white")
-    ball.penup()
-    ball.goto(0, 0)
-    ball.dx = 2
-    ball.dy = -2
-
-    # Pen
-    pen = turtle.Turtle()
-    pen.speed(0)
-    pen.color("white")
-    pen.penup()
-    pen.hideturtle()
-    pen.goto(0, 260)
-    pen.write("Player A: 0           PlayerB: 0",
-              align="center", font=('Courier', 24, "normal"))
-
-    # Winner
-    winner = turtle.Turtle()
-    winner.speed(0)
-    winner.color("white")
-    winner.penup()
-    winner.showturtle()
-    winner.goto(0, 200)
-
-    # Keyboard Binding
-    wn.listen()
-    wn.onkeypress(paddle_a_up, "w")
-    wn.onkeypress(paddle_a_down, "s")
-    wn.onkeypress(paddle_b_up, "Up")
-    wn.onkeypress(paddle_b_down, "Down")
-
-    # Audio
-    # audio = "./assets/sounds/chant.wav"
-    # playAudio = subprocess.call(["afplay", audio])
-
-
 # Functions
 
 
@@ -216,17 +149,35 @@ while True:
         ball.dx *= -1
         # playAudio
 
-    # Announce winner
+    # Announce winner and reset
     if score_a == 5:
         winner.write("The winner is Player A", align="center",
                      font=('Courier', 24, "normal"))
+
         time.sleep(10)
-        wn.reset()
-        # setScreenAttributes()
+        score_a = 0
+        score_b = 0
+        pen.clear()
+        pen.write("Player A: {}       PlayerB: {}".format(
+            score_a, score_b), align="center", font=('Courier', 24, "normal"))
+        winner.clear()
+        paddle_a.goto(-350, 0)
+        paddle_b.goto(350, 0)
+        ball.goto(0, 0)
+        ball.dx *= -1
 
     if score_b == 5:
-        winner.write("The winner is Player A", align="center",
+        winner.write("The winner is Player B", align="center",
                      font=('Courier', 24, "normal"))
+
         time.sleep(10)
-        wn.reset()
-        # setScreenAttributes()
+        score_a = 0
+        score_b = 0
+        pen.clear()
+        pen.write("Player A: {}       Player B: {}".format(
+            score_a, score_b), align="center", font=('Courier', 24, "normal"))
+        winner.clear()
+        paddle_a.goto(-350, 0)
+        paddle_b.goto(350, 0)
+        ball.goto(0, 0)
+        ball.dx *= -1
